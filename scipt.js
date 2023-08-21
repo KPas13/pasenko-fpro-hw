466ee21a9dce23d3899b2889920171114f03e0da
KPas13 <kpasenko64@gmail.com>
2023-08-21 23:03:57 +0300

    add javascript file with code for working with loops

---
+++ scipt.js
@@ -0,0 +1,112 @@
+// Task 1
+let i = 10;
+let result = "";
+
+do {
+    result += i + " ";
+    i++;
+} while (i >= 10 && i <= 20);
+
+console.log(result);
+
+// Task 2
+// let i = 10;
+// let result2 = "";
+// let result2 = 0;
+// do{
+//     console.log(i * i);
+//     i++;
+// }while (i >= 10 && i <= 20)
+
+// Task 3
+// for (let i = 1; i < 11; i++){
+//     console.log(`7 * ${i} = ` + (7 * i));
+// }
+
+// Task 4
+// let result4 = 0;
+// for(let i = 0; i < 16; i++){
+//     result3 += i;
+// }
+// console.log(result3);
+
+// Task 5
+// let result5 = 1;
+// for(let i = 15; i < 36; i++){
+//     result5 *= i;
+// }
+// console.log(result5);
+
+// Task 6
+// let average = 0;
+// let sum = 0;
+// for(let i = 1; i <= 500; i++){
+//     sum += i;
+//     average = sum / 500;
+// }
+// console.log(average);
+
+// Task 7
+// let sum = 0;
+// for(let i = 30; i <= 80; i++){
+//     if(i % 2 === 0){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// Task 8
+// for(let i = 100; i <= 200; i++){
+//     if(i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+// Task 9
+// let num = +prompt("Введіть натуральне число: ", "1");
+// while (isNaN(String(Number(num))) || num <= 0){
+//     num = +prompt("Введіть натуральне число: ", "1");
+// }
+//
+// for(let i = 1; i <= num; i++){
+//     if( num % i === 0){
+//         console.log(i);
+//     }
+// }
+
+// Task 10
+// let num2 = +prompt("Введіть натуральне число: ", "1");
+// let counter = 0;
+// while (isNaN(String(Number(num2))) || num2 <= 0){
+//     num2 = +prompt("Введіть натуральне число: ", "1");
+// }
+//
+// for(let i = 1; i <= num2; i++){
+//     if( num2 % i === 0 && i % 2 === 0){
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+// Task 11
+// let num2 = +prompt("Введіть натуральне число: ", "1");
+// let sum = 0;
+// while (isNaN(String(Number(num2))) || num2 <= 0){
+//     num2 = +prompt("Введіть натуральне число: ", "1");
+// }
+//
+// for(let i = 1; i <= num2; i++){
+//     if( num2 % i === 0 && i % 2 === 0){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// Task 12
+// for (let i = 1; i < 11; i++){
+//     for(let j = 1; j < 11; j++){
+//         console.log(`${i} * ${j} = ` + (i * j));
+//     }
+// }
+
+

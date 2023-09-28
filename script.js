ca29c65ec1ff82fe8ded4f5c828d7f7c3c6a8a61
KPas13 <kpasenko64@gmail.com>
2023-09-28 23:16:37 +0300

    add code for homework 30

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstPage = document.querySelector('#first-page');
+let secondPage = document.querySelector('#second-page');
+
+function linkProtocoleCheck(event) {
+    let linkToPage = event.target.getAttribute('href');
+    console.log(linkToPage);
+    if (!linkToPage.startsWith('http://') || !linkToPage.startsWith('https://')) {
+        linkToPage = "http://" + linkToPage;
+    }
+    window.open(linkToPage, '_blank');
+}
+
+firstPage.addEventListener('click', linkProtocoleCheck);
+secondPage.addEventListener('click', linkProtocoleCheck);
+
+
+
+
+
